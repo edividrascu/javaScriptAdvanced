481e2770fe462f3d06fbf913a5291fb2b6630191
edi vidrascu <edividrascu@gmail.com>
2023-11-16 20:37:54 +0100

    clousures eje3

--- Closures/exercise3/exercise.js
+++ Closures/exercise3/exercise.js
@@ -1,3 +1,12 @@
 function multiplyByTwo(value) {
   let number = 2;
+  
+  function inner() {
+    let resut =
+    number * value;
+    return resut;
+  }
+  return inner;
 }
+console.log(multiplyByTwo(4)()) //4x2 = 8
+
