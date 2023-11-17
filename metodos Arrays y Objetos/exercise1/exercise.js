4512baf6639b27a3eacec3b0aeff03a8268706ad
edi vidrascu <edividrascu@gmail.com>
2023-11-17 16:52:56 +0100

    ejercicio 1 arrays

---
+++ metodos Arrays y Objetos/exercise1/exercise.js
@@ -0,0 +1,25 @@
+// Create a function called `adultFilter` that takes in an array of people and returns the ones who are of age. //mayores de edad
+
+function adultFilter(persons) {
+  let adults = persons.filter((person) => {
+    return person.age >= 18;
+  });
+  return adults;
+}
+
+const persons = [
+  { name: "Paul", age: 16 },
+  { name: "George", age: 17 },
+  { name: "Lucas", age: 21 },
+  { name: "Marco", age: 32 },
+  { name: "Peter", age: 18 },
+  { name: "Carl", age: 13 },
+  { name: "Simon", age: 24 },
+  { name: "Mark", age: 15 },
+  { name: "Sandra", age: 34 },
+  // { name: "Alice", age: 28 },
+];
+
+const adults = adultFilter(persons);
+console.log(persons);
+console.log(adults);
