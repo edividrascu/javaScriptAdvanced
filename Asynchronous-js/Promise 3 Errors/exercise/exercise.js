664d612e224f514703c58808810ba42d0ddbefc8
edi vidrascu <edividrascu@gmail.com>
2023-12-08 13:31:16 +0100

    todos hechos

--- Asynchronous-js/Promise 3 Errors/exercise/exercise.js
+++ Asynchronous-js/Promise 3 Errors/exercise/exercise.js
@@ -1,1 +1,37 @@
+const isLogged = true;
 
+function miPromise(isLogged) {
+    return new Promise((risolve,reject)=>{
+        if(isLogged==true){
+            setTimeout(() => {
+                risolve(Math.random())
+            }, 2000); 
+        }else{
+            reject(new Error("servidor no responde"))
+        }
+    })
+}
+
+function promsie2(number) {
+    return new Promise ((resolve,reject)=>{
+        if (number>=0.5) {
+            resolve({name: "John", age: 24})
+        }else{
+            reject(new Error("servidor no responde"))
+        }
+    })
+}
+
+miPromise(isLogged)
+.then((randomNumber)=>{
+    console.log(randomNumber)
+   return promsie2(randomNumber)
+   
+})
+.then((user)=>{
+    console.log(user)
+})
+.catch((err)=>{
+    console.log(err)
+})
+.finally(()=>console.log("promesa finally"))
--- Asynchronous-js/Promise1/exercise/exercise.js
+++ Asynchronous-js/Promise1/exercise/exercise.js
@@ -1,1 +1,17 @@
-const number = 15;
+const number = 8;
+
+let miPromise = new Promise ((resolve,reject)=>{
+    if (number>=10){
+        resolve("ES mayor que 10")
+    }else {
+        reject("es menor de 10")
+    }
+})
+
+miPromise
+.then((done)=>{
+    console.log(done)
+})
+.catch((err)=>{
+  console.log(err)
+})
